7cac787ca6e54c3839db53cd5af6da0fe808938d
Hazem-anwar98 <a.hazem@bonus.sa>
2025-05-28 16:15:18 +0300

    add markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,13 +1,15 @@
+import StatusBadge from "@/app/components/statusBadge";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
 import React from "react";
+import ReactMarkdown from "react-markdown";
 
 interface Props {
   params: { id: string };
 }
 
 const IssueDetailsPage = async ({ params }: Props) => {
-  if (typeof params.id !== "number") notFound();
+  //   if (typeof params.id !== "number") notFound();
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -15,10 +17,17 @@ const IssueDetailsPage = async ({ params }: Props) => {
   if (!issue) notFound();
   return (
     <div className="container mx-auto">
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <div className="issue-box rounded-md border border-[#ccc] max-w-xl">
+        <p className="p-3 border-b border-[#ccc] flex items-center justify-between">
+          {issue.title}
+          <StatusBadge status={issue.status}></StatusBadge>
+        </p>
+        <div className="p-3 border-b border-[#ccc] prose">
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </div>
+
+        <p className="p-3 ">{issue.createdAt.toDateString()}</p>
+      </div>
     </div>
   );
 };
