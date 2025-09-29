7166d9724a533ec5e59d48f98aeb6c2f13dc83e7
Hazem-anwar98 <a.hazem@bonus.sa>
2025-09-29 20:17:11 +0300

    fix Request

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -49,7 +49,7 @@ export async function PATCH(
 }
 
 export async function DELETE(
-  request: NextRequest,
+  request: Request,
   { params }: { params: { id: string } }
 ) {
   const session = await getServerSession(authOptions);
